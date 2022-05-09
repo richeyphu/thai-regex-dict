98c78a3cf3ef0587538f1075cd8208b141f2f504
AkaraSellegg <55230837+richeyphu@users.noreply.github.com>
2022-05-10 01:13:05 +0700

    Create ScrollButton component

---
+++ src/components/ScrollButton.tsx
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+import { Button } from "@chakra-ui/react";
+import * as Icon from "react-feather";
+
+const ScrollButton = () => {
+  const [visible, setVisible] = useState(false);
+
+  const toggleVisible = () => {
+    const scrolled = document.documentElement.scrollTop;
+    if (scrolled > 300) {
+      setVisible(true);
+    } else if (scrolled <= 300) {
+      setVisible(false);
+    }
+  };
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+      /* you can also use 'auto' behaviour
+         in place of 'smooth' */
+    });
+  };
+
+  window.addEventListener("scroll", toggleVisible);
+
+  return (
+    <Button mt={6}>
+      <Icon.ArrowUp
+        onClick={scrollToTop}
+        style={{ display: visible ? "inline" : "none" }}
+      />
+    </Button>
+  );
+};
+
+export default ScrollButton;
