f88a8cde14092fc34e64fd3b1f109dd1fae1b020
AkaraSellegg <55230837+richeyphu@users.noreply.github.com>
2022-05-09 23:21:26 +0700

    Implement chakra-ui

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,13 +5,18 @@ import Head from "next/head";
 // import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
+import { Button, Flex, Heading, Input, useColorMode, useColorModeValue } from "@chakra-ui/react";
+
 import ThaiWordlist from "../common/thai-wordlist.json";
 
 const Home: NextPage = () => {
-  const [value, setValue] = useState("");
+  const [value, setValue] = useState<string>("");
+
+  const { toggleColorMode } = useColorMode();
+  const formBackground = useColorModeValue("gray.100", "gray.700");
 
   const search = () => {
-    const regex: RegExp = RegExp(value)
+    const regex: RegExp = RegExp(value);
     console.log(ThaiWordlist.filter((word: string) => word.match(regex)));
     // console.log(ThaiWordlist.filter((word: string) => word.includes(value)));
   };
@@ -24,7 +29,26 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
+      <Flex height="100vh" alignItems="center" justifyContent="center">
+        <Flex direction="column" background={formBackground} p={12} rounded={6}>
+          <Heading mb={6}>Thai Regex Dict</Heading>
+          <Input
+            placeholder="ค้นหา..."
+            mb={3}
+            type="text"
+            value={value}
+            onChange={(e) => {
+              setValue(e.currentTarget.value);
+            }}
+          />
+          <Button mb={6} colorScheme="teal" onClick={search}>
+            Search
+          </Button>
+          <Button onClick={toggleColorMode}>Dark/Light</Button>
+        </Flex>
+      </Flex>
+
+      {/* <main className={styles.main}>
         <h1 className={styles.title}>Thai Regex Dict</h1>
 
         <div>
@@ -41,7 +65,7 @@ const Home: NextPage = () => {
             Search
           </button>
         </div>
-      </main>
+      </main> */}
 
       {/* <footer className={styles.footer}>
         <a
