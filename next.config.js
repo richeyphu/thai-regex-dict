3536cc0f9e4f17d98e1b21b61287a64b45e6e2f7
AkaraSellegg <55230837+richeyphu@users.noreply.github.com>
2023-02-16 19:33:18 +0700

    Update dependencies

--- next.config.js
+++ next.config.js
@@ -1,16 +1,16 @@
-const withPWA = require("next-pwa");
 const withPreact = require("next-plugin-preact");
+const withPWA = require("next-pwa")({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development",
+});
 
 /** @type {import('next').NextConfig} */
 const nextConfig = withPWA(
   withPreact({
     reactStrictMode: true,
-    pwa: {
-      dest: "public",
-      register: true,
-      skipWaiting: true,
-      disable: process.env.NODE_ENV === "development",
-    },
+    swcMinify: true,
   })
 );
 
