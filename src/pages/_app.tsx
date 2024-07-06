137ffbb93a540350645ebeb647ec78d415ec5864
akara <55230837+richeyphu@users.noreply.github.com>
2024-07-07 03:03:05 +0700

    Install vercel analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,18 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 
+import { Analytics } from "@vercel/analytics/react";
 import { ChakraProvider } from "@chakra-ui/react";
 import theme from "@theme";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider theme={theme}>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <>
+      <ChakraProvider theme={theme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
+      <Analytics />
+    </>
   );
 }
 
